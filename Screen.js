4328836f92dd363f594e6212b2cab91c2aa74aee
iipeter01430 <60205451+iipeter01430@users.noreply.github.com>
2020-02-17 15:51:29 -0700

    Update Screen.js

--- Screen.js
+++ Screen.js
@@ -43,3 +43,18 @@ basic.forever(function () {
     ball.ifOnEdgeBounce();
     ball.move(1);
 })
+
+let awakeMode: boolean = true
+input.onButtonPressed(Button.A, function () {
+    awakeMode = false
+})
+input.onButtonPressed(Button.B, function () {
+    awakeMode = true
+})
+basic.forever(function () {
+    if (awakeMode) {
+        basic.showString("AWAKE!")
+    } else {
+        basic.showString("SLEEP!")
+    }
+})
